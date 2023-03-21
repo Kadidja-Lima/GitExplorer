2d5c16747a56b6e5385a074c49e7f76eced43c9b
Kadidja Lima <kadidja.larissa@hotmail.com>
2023-03-21 10:29:52 -0300

    first commit

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -51,7 +51,7 @@ export class InputComponent implements OnInit {
 
         this.gitHubApiService.get(formvalue.username).subscribe(
         user => {this.goTo('/rotas/result', { user: formvalue.username })
-        this.loadingService.show()
+        this.loadingService.hide()
         },
         error => {this.goTo('/rotas/not-found', { user: formvalue.username }),
         this.loadingService.hide()
